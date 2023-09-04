2534cd66aefdc50dcfeb864264d2541e0df85b46
Source-Ze <143101587+Source-Ze@users.noreply.github.com>
2023-09-04 14:23:41 +0300

    Update config.py
    Signed-off-by: Source-Ze <143101587+Source-Ze@users.noreply.github.com>

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/UI_UI_XB")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/UI_XB")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/UI_OS")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
