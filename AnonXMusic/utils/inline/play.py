ea97e2a2c05d6414422730de12d76ca63dc959ca
Source-Ze <143101587+Source-Ze@users.noreply.github.com>
2023-09-05 22:41:06 +0300

    Update play.py
    Signed-off-by: Source-Ze <143101587+Source-Ze@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -54,11 +54,13 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————◉"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="𝒓𝒆𝒔𝒖𝒎𝒆", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="𝒑𝒂𝒖𝒔𝒆", callback_data=f"ADMIN Pause|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="𝒓𝒆𝒑𝒍𝒂𝒚", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[ 
+            InlineKeyboardButton(text="𝒔𝒌𝒊𝒑", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="𝒔𝒕𝒐𝒑", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
@@ -66,7 +68,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="𝒄𝒍𝒐𝒔𝒆")],
     ]
     return buttons
 
@@ -74,13 +76,15 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="𝒓𝒆𝒔𝒖𝒎𝒆", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="𝒑𝒂𝒖𝒔𝒆", callback_data=f"ADMIN Pause|{chat_id}"),
+        ],[
+            InlineKeyboardButton(text="𝒓𝒆𝒑𝒍𝒂𝒚", callback_data=f"ADMIN Replay|{chat_id}"),
+        ],[ 
+            InlineKeyboardButton(text="𝒔𝒌𝒊𝒑", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="𝒔𝒕𝒐𝒑", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="𝒄𝒍𝒐𝒔𝒆")],
     ]
     return buttons
 
