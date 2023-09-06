60b72bfcaaab56eef52531a5ad0e90534551bb76
Source-Ze <143101587+Source-Ze@users.noreply.github.com>
2023-09-06 13:40:30 +0300

    Update config.py
    Signed-off-by: Source-Ze <143101587+Source-Ze@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/UI_XB",
+    "https://github.com/Source-Ze/ZE-MUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
