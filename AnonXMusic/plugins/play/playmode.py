8c1b4ff1d386f22bcc55d55e3df56c21492b92d8
Source-Ze <143101587+Source-Ze@users.noreply.github.com>
2023-09-07 15:44:38 +0300

    Update playmode.py
    Signed-off-by: Source-Ze <143101587+Source-Ze@users.noreply.github.com>

--- AnonXMusic/plugins/play/playmode.py
+++ AnonXMusic/plugins/play/playmode.py
@@ -1,23 +1,14 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
+from AnonXMusic import app
+from AnonXMusic.utils.database import get_playmode, get_playtype, is_nonadmin_chat
+from AnonXMusic.utils.decorators import language
+from AnonXMusic.utils.inline.settings import playmode_users_markup
 from config import BANNED_USERS
-from strings import get_command
-from AnonX import app
-from AnonX.utils.database import (get_playmode, get_playtype,
-                                       is_nonadmin_chat)
-from AnonX.utils.decorators import language
-from AnonX.utils.inline.settings import playmode_users_markup
-
-### Commands
-PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 
 
-@app.on_message(
-    filters.command(PLAYMODE_COMMAND)
-    & filters.group
-    & ~BANNED_USERS
-)
+@app.on_message(filters.command(["playmode", "mode"]) & filters.group & ~BANNED_USERS)
 @language
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
@@ -37,6 +28,6 @@ async def playmode_(client, message: Message, _):
         Playtype = True
     buttons = playmode_users_markup(_, Direct, Group, Playtype)
     response = await message.reply_text(
-        _["playmode_1"].format(message.chat.title),
+        _["play_22"].format(message.chat.title),
         reply_markup=InlineKeyboardMarkup(buttons),
     )
