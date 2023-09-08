06a10c892a9a7d5eafcad9ba69981426f5b7d1f9
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-08 14:52:21 +0300

    Update تاك.py
    Signed-off-by: xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>

--- AnonXMusic/plugins/ZE/تاك.py
+++ AnonXMusic/plugins/ZE/تاك.py
@@ -45,15 +45,15 @@ async def vgdg(client: Client, message: Message):
         
 
 array = []
-@app.on_message(command(["@all", "تاك","تاك للكل"]) & ~filters.private)
+@Client.on_message(filters.command(["@all", "تاك","all"], "") & ~filters.private)
 async def nummmm(client: app, message):
   if message.chat.id in array:
-     return await message.reply_text("**التاك قيد التشغيل حالياً ،**")
+     return await message.reply_text("**التاك قيد التشغيل الان .**")
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if not chek.status in ["administrator", "creator"]:
-    await message.reply("**يجب انت تكون مشرف لاستخدام الامر 🖱️**")
+  if not chek.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+    await message.reply("**عذرا عزيزي هذا الامر للادمن الجروب فقط .**")
     return
-  await message.reply_text("**جاري بدأ المنشن ، لايقاف الامر اضغط **\n /cancel او اكتب بس منشن")
+  await message.reply_text("**جاري بدأ المنشن ، لايقاف الامر اضغط /cancel .**")
   i = 0
   txt = ""
   zz = message.text
@@ -62,17 +62,17 @@ async def nummmm(client: app, message):
           photo = await client.download_media(photo_id)
           zz = message.caption
   try:
-   zz = zz.replace("@all","").replace("تاك","").replace("نادي الكل","")
+   zz = zz.replace("@all","").replace("تاك","").replace("all","")
   except:
     pass
   array.append(message.chat.id)
-  async for x in client.iter_chat_members(message.chat.id):
+  async for x in client.get_chat_members(message.chat.id):
       if message.chat.id not in array:
         return
       if not x.user.is_deleted:
        i += 1
-       txt += f" {x.user.mention} ،"
-       if i == 5:
+       txt += f" {x.user.mention} ›"
+       if i == 20:
         try:
               if not message.photo:
                     await client.send_message(message.chat.id, f"{zz}\n{txt}")
@@ -91,18 +91,18 @@ async def nummmm(client: app, message):
   array.remove(message.chat.id)
 
 
-@app.on_message(command(["بس المنشن", "/cancel","بس منشن"]))
+@Client.on_message(filters.command(["/cancel", "ايقاف التاك"], ""))
 async def stop(client, message):
   chek = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if not chek.status in ["administrator", "creator"]:
-    await message.reply("**يجب انت تكون مشرف لاستخدام الامر 🖱️")
+  if not chek.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+    await message.reply("**عذرا عزيزي هذا الامر للادمن الجروب فقط .**")
     return
   if message.chat.id not in array:
-     await message.reply("**المنشن متوقف بالفعل**")
+     await message.reply("**المنشن متوقف بي الفعل .**")
      return 
   if message.chat.id in array:
     array.remove(message.chat.id)
-    await message.reply("**تم ايقاف المنشن بنجاح✅**")
+    await message.reply("**تم ايقاف المنشن عزيزي .**")
     return
 
 
