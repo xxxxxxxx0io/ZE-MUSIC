cd74f0babced7c78bf75caab6ac3447cb27f4bf6
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-12 13:50:28 +0300

    Update azan.py
    Signed-off-by: xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>

--- AnonXMusic/plugins/ZE/azan.py
+++ AnonXMusic/plugins/ZE/azan.py
@@ -44,9 +44,7 @@ async def kill():
   for i in chat:
     await Anony.force_stop_stream(i)
 
-@app.on_message(filters.regex("^اذن$"))
-async def strcall(client, message):
-def play(i)
+async def play(i)
   assistant = await group_assistant(Anony,i)
   file_path = "./AnonXMusic/assets/azan.mp3"
   stream = AudioPiped(file_path, audio_parameters=HighQualityAudio())
