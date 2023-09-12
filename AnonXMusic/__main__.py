3a2f12e6d31da90546075e59ada6b93b8ecd34bc
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-12 23:51:15 +0300

    Update __main__.py
    Signed-off-by: xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -10,8 +10,8 @@ from AnonXMusic.core.call import Anony
 from AnonXMusic.misc import sudo
 from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
-from AnonXMusic.plugins.ZE.azan import azan
 from config import BANNED_USERS
+from AnonXMusic.plugins.ZE.azan import azkar
 
 
 async def init():
@@ -53,11 +53,12 @@ async def init():
     LOGGER("AnonXMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
+    await azkar()
     await idle()
     await app.stop()
     await userbot.stop()
     LOGGER("AnonXMusic").info("Stopping AnonX Music Bot...")
-    await azan()
-    
+
+
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
