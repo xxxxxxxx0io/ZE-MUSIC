9e5a4b6fb3e84707a91824d23a33f844c3bc50af
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-14 19:27:55 +0300

    Add files via upload
    Signed-off-by: xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>

---
+++ AnonXMusic/plugins/ZE/create&close.py
@@ -0,0 +1,157 @@
+import asyncio
+from typing import Optional
+from strings import get_string
+from random import randint
+from pyrogram.types import Message, ChatPrivileges, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters
+from strings.filters import command
+from pyrogram.raw.functions.channels import GetFullChannel
+from pyrogram.raw.functions.messages import GetFullChat
+from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
+from AnonXMusic.utils.database import *
+from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
+from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant, ChatAdminRequired
+from AnonXMusic import app , Userbot
+from AnonXMusic.utils.decorators import is_Admin
+
+async def get_group_call(
+    client: Client, message: Message, err_msg: str = ""
+) -> Optional[InputGroupCall]:
+    assistant = await get_assistant(message.chat.id)
+    chat_peer = await assistant.resolve_peer(message.chat.id)
+    if isinstance(chat_peer, (InputPeerChannel, InputPeerChat)):
+        if isinstance(chat_peer, InputPeerChannel):
+            full_chat = (
+                await assistant.invoke(GetFullChannel(channel=chat_peer))
+            ).full_chat
+        elif isinstance(chat_peer, InputPeerChat):
+            full_chat = (
+                await assistant.invoke(GetFullChat(chat_id=chat_peer.chat_id))
+            ).full_chat
+        if full_chat is not None:
+            return full_chat.call
+    await app.send_message(f"**No group call Found** {err_msg}")
+    return False
+
+@app.on_message(filters.regex("^افتح الكول$"))
+async def start_group_call(c,msg):
+    chek = await is_Admin(msg.chat.id,msg.from_user.id,msg)
+    if chek == False :
+        await msg.reply("• يجب ان تكون مشرف بالمجموعه لاستخدام الاوامر")
+        return False 
+    chat_id = msg.chat.id
+    assistant = await get_assistant(chat_id)
+    ass = await assistant.get_me()
+    assid = ass.id
+    if assistant is None:
+        await app.send_message(chat_id, "خطأ في المساعد")
+        return
+    msgb = await app.send_message(chat_id, "جاري تشغيل المكالمه..")
+    try:
+        peer = await assistant.resolve_peer(chat_id)
+        await assistant.invoke(
+            CreateGroupCall(
+                peer=InputPeerChannel(
+                    channel_id=peer.channel_id,
+                    access_hash=peer.access_hash,
+                ),
+                random_id=assistant.rnd_id() // 9000000000,
+            )
+        )
+        await msgb.edit_text("تم فتح الكول بنجاح 🦕️~!")
+    except ChatAdminRequired:
+      try:    
+        await app.promote_chat_member(chat_id, assid, privileges=ChatPrivileges(
+                can_manage_chat=False,
+                can_delete_messages=False,
+                can_manage_video_chats=True,
+                can_restrict_members=False,
+                can_change_info=False,
+                can_invite_users=False,
+                can_pin_messages=False,
+                can_promote_members=False,
+            ),
+        )
+        peer = await assistant.resolve_peer(chat_id)
+        await assistant.invoke(
+            CreateGroupCall(
+                peer=InputPeerChannel(
+                    channel_id=peer.channel_id,
+                    access_hash=peer.access_hash,
+                ),
+                random_id=assistant.rnd_id() // 9000000000,
+            )
+        )
+        await app.promote_chat_member(chat_id, assid, privileges=ChatPrivileges(
+            can_manage_chat=False,
+            can_delete_messages=False,
+            can_manage_video_chats=False,
+            can_restrict_members=False,
+            can_change_info=False,
+            can_invite_users=False,
+            can_pin_messages=False,
+            can_promote_members=False,
+            ),
+        )                              
+        await msgb.edit_text("تم فتح الكول بنجاح 🦕️~!")
+      except:
+         await msgb.edit_text("خلي البوت معاه صلاحية اضافة مشرفين والتحكم ف المحادثه الصوتيه او خلي الاك المساعد رول وجرب 🦕")
+@app.on_message(filters.regex("^اقفل الكول$"))
+async def stop_group_call(c,msg):
+    chek = await is_Admin(msg.chat.id,msg.from_user.id,msg)
+    if chek == False :
+        await msg.reply("• يجب ان تكون مشرف بالمجموعه لاستخدام الاوامر")
+        return False
+    chat_id = m.chat.id
+    assistant = await get_assistant(chat_id)
+    ass = await assistant.get_me()
+    assid = ass.id
+    if assistant is None:
+        await app.send_message(chat_id, "خطأ في المساعد")
+        return
+    msg = await app.send_message(chat_id, "جاري اغلاق المكالمه..")
+    try:
+        if not (
+           group_call := (
+               await get_group_call(assistant, m, err_msg=", group call already ended")
+           )
+        ):  
+           return
+        await assistant.invoke(DiscardGroupCall(call=group_call))
+        await msg.edit_text("تم اغلاق الكول بنجاح 🦕️~!")
+    except Exception as e:
+      if "GROUPCALL_FORBIDDEN" in str(e):
+       try:    
+         await app.promote_chat_member(chat_id, assid, privileges=ChatPrivileges(
+                can_manage_chat=False,
+                can_delete_messages=False,
+                can_manage_video_chats=True,
+                can_restrict_members=False,
+                can_change_info=False,
+                can_invite_users=False,
+                can_pin_messages=False,
+                can_promote_members=False,
+             ),
+         )
+         if not (
+           group_call := (
+               await get_group_call(assistant, m, err_msg=", group call already ended")
+           )
+         ):  
+           return
+         await assistant.invoke(DiscardGroupCall(call=group_call))
+         await app.promote_chat_member(chat_id, assid, privileges=ChatPrivileges(
+            can_manage_chat=False,
+            can_delete_messages=False,
+            can_manage_video_chats=False,
+            can_restrict_members=False,
+            can_change_info=False,
+            can_invite_users=False,
+            can_pin_messages=False,
+            can_promote_members=False,
+            ),
+         )                              
+         await msg.edit_text("تم اغلاق الكول بنجاح 🦕️~!")
+       except:
+         await msg.edit_text("خلي البوت معاه صلاحية اضافة مشرفين والتحكم ف المحادثه الصوتيه او خلي الاك المساعد رول وجرب 🦕")
+    
---
+++ AnonXMusic/plugins/ZE/extra.py
@@ -0,0 +1,80 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, ChatMember, InlineKeyboardMarkup, InlineKeyboardButton
+from telegraph import upload_file
+import os
+from strings.filters import command
+from AnonXMusic import app
+from pyrogram import enums
+from pyrogram.enums import ChatMemberStatus
+
+@app.on_message(command(["تلغراف", "تلغراف ميديا", "ميديا"]) & ~filters.edited)
+async def telegraph(client: Client, message: Message):
+    replied = message.reply_to_message
+    if not replied:
+        await message.reply("🤕 ¦ الرد على ملف وسائط مدعوم\n• حط صوره و اكتب عليها 00")
+        return
+    if not (
+        (replied.photo and replied.photo.file_size <= 5242880)
+        or (replied.animation and replied.animation.file_size <= 5242880)
+        or (
+            replied.video
+            and replied.video.file_name.endswith(".mp4")
+            and replied.video.file_size <= 5242880
+        )
+        or (
+            replied.document
+            and replied.document.file_name.endswith(
+                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
+            )
+            and replied.document.file_size <= 5242880
+        )
+    ):
+        await message.reply("غير مدعوم!!")
+        return
+    download_location = await client.download_media(
+        message=message.reply_to_message,
+        file_name="root/downloads/",
+    )
+    try:
+        response = upload_file(download_location)
+    except Exception as document:
+        await message.reply(message, text=document)
+    else:
+        await message.reply(
+            f"<b>• الــرابـط:-</b>\n\n <code>https://telegra.ph{response[0]}</code>",
+            quote=True,
+            reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="🦕 ¦ افـتح الـرابـط", url=f"https://telegra.ph{response[0]}"),
+                    InlineKeyboardButton(text="🧚🏻‍♀️️ ¦ مشـاركه الـرابـط", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                ],
+            ]
+        )
+    )
+    finally:
+        os.remove(download_location)
+    
+@app.on_message(command(["المالك"]))
+async def creator(c,msg):
+    x = []
+    async for m in app.get_chat_members(msg.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+         if m.status == ChatMemberStatus.OWNER:
+            x.append(m.user.id)
+    if len(x) != 0:        
+       lol = await app.get_users(int(x[0]))
+       lol = await app.get_owner_bio(bio) 
+       if lol.photo:
+         async for photo in app.get_chat_photos(x[0],limit=1):
+          await msg.reply_photo(photo.file_id,caption=f"᥆ꪝᥒ꧖ᖇ | - {lol.mention} 🦕\n\nႦᎥ᥆ | - {lol.bio} 🦕",reply_markup=InlineKeyboardMarkup(
+             [              
+               [          
+                 InlineKeyboardButton(lol.first_name, url=f"https://t.me/{lol.username}")
+               ],             
+             ]                 
+            )                     
+          )
+       else:
+        await msg.reply_text(f"᥆ꪝᥒ꧖ᖇ | - {lol.mention} 🦕\n\nႦᎥ᥆ | - {lol.bio} 🦕", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(lol.first_name, url=f"https://t.me/{lol.username}")],]))
+    else:
+        await msg.reply_text("الاك محذوف يقلب")
