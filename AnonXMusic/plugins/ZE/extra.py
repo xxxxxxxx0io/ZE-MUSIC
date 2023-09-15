069ec3c29bffc86ce88844aa53000be98f34eb2c
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-15 16:44:17 +0300

    Update extra.py
    Signed-off-by: xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>

--- AnonXMusic/plugins/ZE/extra.py
+++ AnonXMusic/plugins/ZE/extra.py
@@ -14,7 +14,7 @@ from strings.filters import command
 from pyrogram import enums
 from pyrogram.enums import ChatMemberStatus
 
-@app.on_message(command(["تلغراف", "تلغراف ميديا", "ميديا"]) & ~filters.edited)
+@app.on_message(filters.command(["تلغراف", "تلغراف ميديا", "ميديا"]) & filters.edited & ~BANNED_USERS)
 async def telegraph(client: Client, message: Message):
     replied = message.reply_to_message
     if not replied:
@@ -62,7 +62,7 @@ async def telegraph(client: Client, message: Message):
     finally:
         os.remove(download_location)
     
-@app.on_message(command(["المالك"]))
+@app.on_message(filters.command(["المالك"]))
 async def creator(c,msg):
     x = []
     async for m in app.get_chat_members(msg.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
