7da7fefdef47b327a1d1ed20abceed0aa5ab2459
xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>
2023-09-17 13:57:13 +0300

    Update Sh3'l.py
    Signed-off-by: xxxxxxxx0io <143849771+xxxxxxxx0io@users.noreply.github.com>

--- AnonXMusic/plugins/ZE/Sh3'l.py
+++ AnonXMusic/plugins/ZE/Sh3'l.py
@@ -4,12 +4,26 @@ from pyrogram.errors import FloodWait
 from pyrogram.raw import types
 from AnonXMusic import app
 
+users_list = []
 
-@app.on_message(filters.voice & filters.command(["مين شغل"]))
-async def whose_job(client, message):
-    song_info = message.voice
-    performer = song_info.performer
-    if performer:
-        await message.reply(f"الشخص الذي شغل هذه الأغنية هو  {performer}.")
+@app.on_message(filters.command(["مين شغل", "شغل", "/play"]))
+async def get_last_user(client, message):
+    # التحقق مما إذا كان المستخدم قد قام بكتابة الأمر
+    if message.from_user.username:
+        # إضافة اسم المستخدم والمستخدم إلى قائمة المستخدمين
+        users_list.append((message.from_user.username, message.from_user.first_name))
+        await message.reply(f"تم تسجيل المستخدم: {message.from_user.first_name} (@{message.from_user.username})",
+                            reply_to_message_id=message.message_id)
     else:
-        await message.reply("مفيش أغنية شغالة أصلاً يا صحبي.")
+        await message.reply("عذرًا، لا يمكنني الوصول إلى اسم المستخدم الخاص بك.", reply_to_message_id=message.message_id)
+
+@app.on_message(filters.command(["مين شغل"]))
+async def get_last_user(client, message):
+    if users_list:
+        # الحصول على آخر مستخدم قام بكتابة الأمر
+        last_user = users_list[-1]
+        await message.reply(f"الي شغل اهو: {last_user[1]} (@{last_user[0]})",
+                            reply_to_message_id=message.message_id)
+    else:
+        await message.reply("مفيش حد شغل حاجه.",
+                            reply_to_message_id=message.message_id)
